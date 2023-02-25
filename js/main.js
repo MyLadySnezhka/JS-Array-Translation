14aea84cc3e82815a0af4027e0974d08b9767377
MyLadySnezhka <mylady.snezhka@gmail.com>
2023-02-25 03:05:32 +0200

    small img > big online img

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const elSlider = document.querySelector('.slider');
+const elOnline = document.querySelector('.online');
 
 const imagesArr = [
     '../images/anger.png',
@@ -8,6 +9,10 @@ const imagesArr = [
     '../images/sport.png',
 ]
 
+elOnline.style.backgroundImage = "url('../images/filmsplash.png')";
+
+
+const renderSlider = () => {
 
 const htmlImgArr = imagesArr.map((item) => {
     const _html = `<div class="item" style="background: url('${item}') no-repeat center center; background-size: cover;"></div>`;
@@ -18,11 +23,10 @@ const htmlSlider = htmlImgArr.join(' ');
 
 elSlider.innerHTML = htmlSlider;
 
-const elItem = document.querySelector('.slider > .item');
-const elItem2 = document.querySelectorAll('.slider > .item');
+};
 
-console.log(elItem2);
+renderSlider();
 
-elItem.addEventListener('click', (ev) => {
-    console.log('бум');
+elSlider.addEventListener('click', (ev) => {
+    elOnline.style.backgroundImage = ev.target.style.backgroundImage;
 })
